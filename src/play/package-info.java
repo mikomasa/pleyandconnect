3c60889f83fabe9101ffea68c27b9133ca2868dc
mikomasa <mizuna0119@docomo.ne.jp>
2023-11-07 12:45:55 +0900

    ああああ

--- src/play/package-info.java
+++
@@ -1,9 +0,0 @@
-package play;
-
-public static void main(String args[]) {
-    System.out.print("Hello ");
-    System.out.println("World!");
-    System.out.print("Hello ");
-    System.out.print("Hello ");
-    System.out.println("World!");
-  }
