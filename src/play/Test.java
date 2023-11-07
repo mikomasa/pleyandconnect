9300baba279909456444cb41ded49060496430cd
mikomasa <mizuna0119@docomo.ne.jp>
2023-11-07 12:45:36 +0900

    Test.java作成

---
+++ src/play/Test.java
@@ -0,0 +1,12 @@
+package play;
+
+public class Test {
+	public static void main(String args[]) {
+	    System.out.print("Hello ");
+	    System.out.println("World!");
+	    System.out.print("Hello ");
+	    System.out.print("Hello ");
+	    System.out.println("World!");
+	  }
+
+}
