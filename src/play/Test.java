49dabfcc4c1bb2f48e8a10f4ad2a7c7aa3cf6631
mikomasa <mizuna0119@docomo.ne.jp>
2023-11-07 15:44:52 +0900

    test

--- src/play/Test.java
+++ src/play/Test.java
@@ -2,6 +2,7 @@ package play;
 
 public class Test {
 	public static void main(String args[]) {
+
 	    System.out.print("Hello ");
 	    System.out.println("World!");
 	    System.out.print("Hello ");
